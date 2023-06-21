a850ea8d85d1144b62561006b9b95e72f4291af9
Pablo <Pablo@DESKTOP-I96ENV7>
2023-06-21 16:58:35 -0300

    Subida Inicial

---
+++ src/main/java/com/tsti/DaosTpApplication.java
@@ -0,0 +1,13 @@
+package com.tsti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaosTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaosTpApplication.class, args);
+	}
+
+}
