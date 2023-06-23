2e6cc87bdc3adb12b290a5edcf129959105f1fcb
Pablo <Pablo@DESKTOP-I96ENV7>
2023-06-23 17:16:59 -0300

    Actualizacion ClienteDAO

---
+++ src/main/java/com/tsti/accesoADatos/ClienteDao.java
@@ -0,0 +1,17 @@
+package com.tsti.accesoADatos;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.tsti.entidades.Cliente;
+
+public interface ClienteDao extends JpaRepository<Cliente, Long>{
+	
+	@Query("SELECT c FROM Cliente c WHERE c.nombre like '%?1%'")
+	Collection<Cliente> findPersonasLike(String parte);
+	
+	public List<Cliente> findByApellidoOrNombre(String apellido, String nombre);
+}
